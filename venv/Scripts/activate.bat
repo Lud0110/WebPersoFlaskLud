569211b15db26c492e38bea1d94bff35fc95eb13
Lud0110 <pumalud0110@gmail.com>
2022-08-29 16:05:22 -0500

    Blueprint home

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ZXS\Desktop\consola\webPersonalflaskLud\venv
+set VIRTUAL_ENV=C:\Users\Lety\Downloads\ProyectoFlaskLud\webPersonalflaskLud\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
--- web_personalLud/app.py
+++ web_personalLud/app.py
@@ -1,67 +1,61 @@
-from flask import Flask, render_template, request
+from tokenize import Number
+from flask import Flask, render_template, request, url_for
+from flask_wtf import FlaskForm
+from wtforms import (StringField, PasswordField, 
+                    SubmitField, EmailField, 
+                    IntegerField, RadioField, 
+                    SelectField, TextAreaField)
+from wtforms.validators import DataRequired, Email
+from home.views import home_blueprint
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
 
 
-######### Rutas Public #######
-@app.route('/')
-def index():
-    return render_template('public/index.html') 
+################### Formularios de WTForms ##############
+class LoginForm(FlaskForm):
+    email = EmailField('Correo', validator=[DataRequired(), Email()])
+    password = PasswordField('Password', validator=[DataRequired()])
+    sumbit = SubmitField('Ingresar')
 
-@app.route('/about')
-def about():
-    return render_template('public/about.html')
+class RegisterForm(FlaskForm):
+    name = StringField('Nombre')
+    last_name =StringField('Apellidos')
+    email = EmailField('Correo')
+    password = PasswordField('Contraseña')
+    phone = IntegerField('Telefono')
+    is_married = RadioField('Estado Civil', choices=[{'True', 'Casado'}, {'False', 'Soltero'}])
+    gender = SelectField('Genero', choices=[{'male', 'Masculino'}, {'female', 'Femenino'}, {'other', 'Otro'}])
+    submit = SubmitField('Registrar')  
 
-@app.route('/contact')
-def contact():
-    return render_template('public/contact.html')
-
-@app.route('/portfolio') 
-def portfolio():
-    projects = [
-        {
-            'name':'Primer proyecto',
-            'description':'As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm....',
-            'image':'img/home-bg.jpg',
-            'url': 'https://www.google.com'
-        },
-         {
-            'name':'Segundo proyecto',
-            'description':'As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm....',
-            'image':'img/about-bg.jpg',
-            'url': 'https://www.xataka.com'
-        },
-        {
-            'name':'Segundo proyecto',
-            'description':'As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm....',
-            'image':'img/about-bg.jpg',
-            'url': 'https://www.xataka.com'
-        },
-        {
-            'name':'Segundo proyecto',
-            'description':'As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm....',
-            'image':'img/about-bg.jpg',
-            'url': 'https://www.xataka.com'
-        },
-    ]
-    return render_template('public/portfolio.html', projects=projects)
-
-###### Rutas ###### 
-@app.route('/auth/login')
+###### Rutas Login ###### 
+@app.route('/auth/login', methods=['GET', 'POST'])
 def login():
-    return render_template('auth/login.html')
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+
+        return render_template('admin/index.html', email=email)
+        
+    return render_template('auth/login.html', form=form)
 
 @app.route('/auth/register')
 def register():
-    return render_template('aunth/register.html') 
 
-@app.route('welcome') 
-def welcome():
-    email = request.args.get('mail')
-    password = request.args.get('password')
-    acces = {'email': email, 'password':password}
+    form = RegisterForm()
 
-    return render_template('admin/index.html', user_acces=acces)         
+    return render_template('auth/register.html', form=form) 
+  
+
+@app.errorhandler(404)
+def page_error_not_found(e):
+    return render_template('error/404.html'), 404    
+
+
+############ Apps ############
+app.register_blueprint(home_blueprint,)     
 
 if __name__ == '__main__':
     app.run(debug=True)               
