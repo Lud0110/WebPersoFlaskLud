e415ec7fdc64ef9d20a3f9f91d7be4de039afc06
Lud0110 <pumalud0110@gmail.com>
2022-08-31 00:44:30 -0500

    Creando la conexion a MySQL

--- web_personalLud/app.py
+++ web_personalLud/app.py
@@ -1,22 +1,30 @@
 ######## imports Flask y Python ########
-from flask import Flask, render_template, request, url_for
+from flask import Flask
 
 ######### Import Apps Blueprints ##########
 from home.views import home_blueprint
 from auth.views import auth_blueprint
+from error_pages.handlers import error_pages_blueprint
+
+from db.db_connection import get_connection
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret'
-  
+app.config['SECRET_KEY'] = 'secret' 
+app.config['ENV'] = 'development'
 
-@app.errorhandler(404)
-def page_error_not_found(e):
-    return render_template('error/404.html'), 404    
+@app.route('/usuarios')
+def usuarios():
+    connection = get_connection
+    with connection.cursor() as cursor:
+        cursor.execute('SELECT' * FROM categorias)
+        usuarios = cursor.fetchall()
+        return str(usuarios)
 
 
 ############ registro de Apps ############
 app.register_blueprint(home_blueprint) 
-app.register_blueprint(auth_blueprint, url_prefix='/auth')    
+app.register_blueprint(auth_blueprint, url_prefix='/auth')
+app.register_blueprint(error_pages_blueprint)    
 
 if __name__ == '__main__':
     app.run(debug=True)               
---
+++ web_personalLud/db/db_connection.py
@@ -0,0 +1,10 @@
+import pymysql
+
+def get_connection():
+    connection = pymysql.connect(host='localhost',
+                                    user='root',
+                                    password='example',
+                                    db='platzi_blog',
+                                    cursorclass=pymysql.cursorclass.DitCursor)
+
+return connection                                       
